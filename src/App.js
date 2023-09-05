07b273d55b2ecec0595cb5caf6a0eb3519dd09e1
Deepanshu Gupta <deepanshugupta899@gmail.com>
2023-09-05 13:55:26 +0530

    dailycommit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 import MyComponent from './MyComponent';
 
 function App() {
   return (
     <>
-    <h1>app</h1>
+    {/* <h2>dfdf</h2> */}
  <MyComponent/>
     </>
   );
